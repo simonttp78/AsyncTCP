cea1a976fc3421bffa247cdb4b6d8f66b3372fc6
BlueAndi <web@blue-andi.de>
2020-04-14 20:50:27 +0200

    Possible NULL pointer fixed.

--- src/AsyncTCP.cpp
+++ src/AsyncTCP.cpp
@@ -232,111 +232,141 @@ static bool _start_async_task(){
 
 static int8_t _tcp_clear_events(void * arg) {
     lwip_event_packet_t * e = (lwip_event_packet_t *)malloc(sizeof(lwip_event_packet_t));
-    e->event = LWIP_TCP_CLEAR;
-    e->arg = arg;
-    if (!_prepend_async_event(&e)) {
-        free((void*)(e));
+
+    if (NULL != e) {
+        e->event = LWIP_TCP_CLEAR;
+        e->arg = arg;
+        if (!_prepend_async_event(&e)) {
+            free((void*)(e));
+        }
     }
+
     return ERR_OK;
 }
 
 static int8_t _tcp_connected(void * arg, tcp_pcb * pcb, int8_t err) {
-    //ets_printf("+C: 0x%08x\n", pcb);
     lwip_event_packet_t * e = (lwip_event_packet_t *)malloc(sizeof(lwip_event_packet_t));
-    e->event = LWIP_TCP_CONNECTED;
-    e->arg = arg;
-    e->connected.pcb = pcb;
-    e->connected.err = err;
-    if (!_prepend_async_event(&e)) {
-        free((void*)(e));
+    //ets_printf("+C: 0x%08x\n", pcb);
+
+    if (NULL != e) {
+        e->event = LWIP_TCP_CONNECTED;
+        e->arg = arg;
+        e->connected.pcb = pcb;
+        e->connected.err = err;
+        if (!_prepend_async_event(&e)) {
+            free((void*)(e));
+        }
     }
+
     return ERR_OK;
 }
 
 static int8_t _tcp_poll(void * arg, struct tcp_pcb * pcb) {
-    //ets_printf("+P: 0x%08x\n", pcb);
     lwip_event_packet_t * e = (lwip_event_packet_t *)malloc(sizeof(lwip_event_packet_t));
-    e->event = LWIP_TCP_POLL;
-    e->arg = arg;
-    e->poll.pcb = pcb;
-    if (!_send_async_event(&e)) {
-        free((void*)(e));
+    //ets_printf("+P: 0x%08x\n", pcb);
+
+    if (NULL != e) {
+        e->event = LWIP_TCP_POLL;
+        e->arg = arg;
+        e->poll.pcb = pcb;
+        if (!_send_async_event(&e)) {
+            free((void*)(e));
+        }
     }
+
     return ERR_OK;
 }
 
 static int8_t _tcp_recv(void * arg, struct tcp_pcb * pcb, struct pbuf *pb, int8_t err) {
     lwip_event_packet_t * e = (lwip_event_packet_t *)malloc(sizeof(lwip_event_packet_t));
-    e->arg = arg;
-    if(pb){
-        //ets_printf("+R: 0x%08x\n", pcb);
-        e->event = LWIP_TCP_RECV;
-        e->recv.pcb = pcb;
-        e->recv.pb = pb;
-        e->recv.err = err;
-    } else {
-        //ets_printf("+F: 0x%08x\n", pcb);
-        e->event = LWIP_TCP_FIN;
-        e->fin.pcb = pcb;
-        e->fin.err = err;
-        //close the PCB in LwIP thread
-        AsyncClient::_s_lwip_fin(e->arg, e->fin.pcb, e->fin.err);
+
+    if (NULL != e) {
+        e->arg = arg;
+        if(pb){
+            //ets_printf("+R: 0x%08x\n", pcb);
+            e->event = LWIP_TCP_RECV;
+            e->recv.pcb = pcb;
+            e->recv.pb = pb;
+            e->recv.err = err;
+        } else {
+            //ets_printf("+F: 0x%08x\n", pcb);
+            e->event = LWIP_TCP_FIN;
+            e->fin.pcb = pcb;
+            e->fin.err = err;
+            //close the PCB in LwIP thread
+            AsyncClient::_s_lwip_fin(e->arg, e->fin.pcb, e->fin.err);
+        }
+        if (!_send_async_event(&e)) {
+            free((void*)(e));
+        }
     }
-    if (!_send_async_event(&e)) {
-        free((void*)(e));
-    }
+
     return ERR_OK;
 }
 
 static int8_t _tcp_sent(void * arg, struct tcp_pcb * pcb, uint16_t len) {
-    //ets_printf("+S: 0x%08x\n", pcb);
     lwip_event_packet_t * e = (lwip_event_packet_t *)malloc(sizeof(lwip_event_packet_t));
-    e->event = LWIP_TCP_SENT;
-    e->arg = arg;
-    e->sent.pcb = pcb;
-    e->sent.len = len;
-    if (!_send_async_event(&e)) {
-        free((void*)(e));
+    //ets_printf("+S: 0x%08x\n", pcb);
+
+    if (NULL != e) {
+        e->event = LWIP_TCP_SENT;
+        e->arg = arg;
+        e->sent.pcb = pcb;
+        e->sent.len = len;
+        if (!_send_async_event(&e)) {
+            free((void*)(e));
+        }
     }
+
     return ERR_OK;
 }
 
 static void _tcp_error(void * arg, int8_t err) {
-    //ets_printf("+E: 0x%08x\n", arg);
     lwip_event_packet_t * e = (lwip_event_packet_t *)malloc(sizeof(lwip_event_packet_t));
-    e->event = LWIP_TCP_ERROR;
-    e->arg = arg;
-    e->error.err = err;
-    if (!_send_async_event(&e)) {
-        free((void*)(e));
+    //ets_printf("+E: 0x%08x\n", arg);
+
+    if (NULL != e) {
+        e->event = LWIP_TCP_ERROR;
+        e->arg = arg;
+        e->error.err = err;
+        if (!_send_async_event(&e)) {
+            free((void*)(e));
+        }
     }
 }
 
 static void _tcp_dns_found(const char * name, struct ip_addr * ipaddr, void * arg) {
     lwip_event_packet_t * e = (lwip_event_packet_t *)malloc(sizeof(lwip_event_packet_t));
     //ets_printf("+DNS: name=%s ipaddr=0x%08x arg=%x\n", name, ipaddr, arg);
-    e->event = LWIP_TCP_DNS;
-    e->arg = arg;
-    e->dns.name = name;
-    if (ipaddr) {
-        memcpy(&e->dns.addr, ipaddr, sizeof(struct ip_addr));
-    } else {
-        memset(&e->dns.addr, 0, sizeof(e->dns.addr));
-    }
-    if (!_send_async_event(&e)) {
-        free((void*)(e));
+
+    if (NULL != e) {
+        e->event = LWIP_TCP_DNS;
+        e->arg = arg;
+        e->dns.name = name;
+        if (ipaddr) {
+            memcpy(&e->dns.addr, ipaddr, sizeof(struct ip_addr));
+        } else {
+            memset(&e->dns.addr, 0, sizeof(e->dns.addr));
+        }
+        if (!_send_async_event(&e)) {
+            free((void*)(e));
+        }
     }
 }
 
 //Used to switch out from LwIP thread
 static int8_t _tcp_accept(void * arg, AsyncClient * client) {
     lwip_event_packet_t * e = (lwip_event_packet_t *)malloc(sizeof(lwip_event_packet_t));
-    e->event = LWIP_TCP_ACCEPT;
-    e->arg = arg;
-    e->accept.client = client;
-    if (!_prepend_async_event(&e)) {
-        free((void*)(e));
+
+    if (NULL != e) {
+        e->event = LWIP_TCP_ACCEPT;
+        e->arg = arg;
+        e->accept.client = client;
+        if (!_prepend_async_event(&e)) {
+            free((void*)(e));
+        }
     }
+
     return ERR_OK;
 }
 
@@ -706,12 +736,12 @@ bool AsyncClient::connect(IPAddress ip, uint16_t port){
 
 bool AsyncClient::connect(const char* host, uint16_t port){
     ip_addr_t addr;
-    
+
     if(!_start_async_task()){
       log_e("failed to start task");
       return false;
     }
-    
+
     err_t err = dns_gethostbyname(host, &addr, (dns_found_callback)&_tcp_dns_found, this);
     if(err == ERR_OK) {
         return connect(IPAddress(addr.u_addr.ip4.addr), port);
